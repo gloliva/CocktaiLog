8ee772ca299964f31212d114bf4a764e91e77a06
Gregg Oliva <gloliva@proton.me>
2024-01-16 14:38:14 -0800

    Update sqlite3 db location to be cocktailog/etc

--- cocktailog/db/defs.py
+++ cocktailog/db/defs.py
@@ -9,7 +9,7 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 
 # Database
 DB_NAME = "cocktailog.db"
-DB_DIRECTORY = os.path.join(PATH, "../..", "meta")
+DB_DIRECTORY = os.path.join(PATH, "../..", "etc")
 DB_FILEPATH = os.path.join(DB_DIRECTORY, DB_NAME)
 
 SQLITE_ENGINE = f"sqlite:///{DB_FILEPATH}"
