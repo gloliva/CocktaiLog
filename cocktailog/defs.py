053d9a876ea52aca0deae755f1231a0fe63496f9
Gregg Oliva <gloliva@proton.me>
2024-01-18 21:07:16 -0800

    Fix defs filepaths

--- cocktailog/defs.py
+++ cocktailog/defs.py
@@ -9,12 +9,12 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 
 # Database
 DB_NAME = "cocktailog.db"
-DB_DIRECTORY = os.path.join(PATH, "../..", "etc")
+DB_DIRECTORY = os.path.join(PATH, "..", "etc")
 DB_FILEPATH = os.path.join(DB_DIRECTORY, DB_NAME)
 
 SQLITE_ENGINE = f"sqlite:///{DB_FILEPATH}"
 
 # Styles
-STYLES_DIRECTORY = os.path.join(PATH, "../..", "styles")
+STYLES_DIRECTORY = os.path.join(PATH, "..", "styles")
 STYLES_FILE = "cocktailog.tcss"
 STYLES_FILEPATH = os.path.join(STYLES_DIRECTORY, STYLES_FILE)
